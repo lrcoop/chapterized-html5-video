0b43ed13a9443bf45f91078f1f3be946c8eea31b
Cooper, Lauren <lauren.cooper@grey.com>
2015-03-26 14:04:21 -0400

    created prev/next button functionality; added in current chapter indicator, current time indicator, and duration indicator; styled some of the basic buttons

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ $(function() {
 		if(Modernizr.video) {
 			var duration,
 				flags = {
-					'initial' : false,
-					'quarter' :false,
-					'half': false,
+					'first' : false,
+					'second' :false,
 					'third': false,
+					'fourth': false,
+					'fifth': false,
 					'finished': false
 				},
 				chapters = {
@@ -18,15 +19,26 @@ $(function() {
 					'15' : 'chapter-4',
 					'20' : 'chapter-5'
 				},
+
+
+				// array = $.map(chapters, function(value, index) {
+				//     return [value];
+				// }),
+				// arrayLength = array.length,
+
+				currentChapter = 0;
+
 				chapterClicker = function(i) {
 					$('.' + chapters[key]).on('click', function(){
 						myVideo.currentTime = i;
+						currentChapter = i;
 						myVideo.play();
 						$('video').addClass('playing');
-
-
 					});
+
 				};
+
+
 				for (var key in chapters) {
 					chapterClicker(key);
 
@@ -37,12 +49,15 @@ $(function() {
 					myVideo.currentTime = 0;
 
 				};
+
 				myVideo.ontimeupdate = function() {
 					trackVideoPlay('video', myVideo.currentTime, duration, chapters);
+		     	onTrackedVideoFrame(currentChapter, this.duration);
+
 				};
+
 				myVideo.onended = function() {
 					myVideo.currentTime = 0;
-					$('.chapter-1').removeClass('current');
 				}
 
 				$('video').on('click', function(){
@@ -56,6 +71,15 @@ $(function() {
 				});
 		}
 
+		function onTrackedVideoFrame(currentChapter, currentTime, duration){
+			currentTime = Math.floor(myVideo.currentTime);
+			duration = Math.floor(myVideo.duration);
+			currentChapter = currentChapter.replace(/\-/g, " ").toUpperCase();
+    	$("#current-chapter").text(currentChapter);
+    	$('#current').text(currentTime);
+    	$("#duration").text(duration);
+		}
+
 		function trackVideoPlay(videoType, progress, duration, chapters) {
 
 			progress = Math.floor(progress);
@@ -63,46 +87,80 @@ $(function() {
 
 			switch(progress){
 				case duration*0:
-					if (flags.initial === false){
-						console.log('initial');
-						flags.initial = true;
+					if (flags.first === false){
+						console.log('chapter-1');
+						flags.first = true;
 					}
 					break;
-				case Math.floor(duration*.25):
-					if (flags.quarter === false){
-						console.log('quarter');
-						flags.quarter = true;
+
+				case Math.floor(duration*.20):
+					if (flags.second === false){
+						console.log('chapter-2');
+						flags.second = true;
 					}
 
 
 					break;
-				case Math.floor(duration*.5):
-				if (flags.half === false){
-					console.log('half');
-					flags.half = true;
+				case Math.floor(duration*.40):
+					if (flags.third === false){
+						console.log('chapter-3');
+						flags.third = true;
+					}
+
+					break;
+				case Math.floor(duration*.60):
+					if (flags.fourth === false){
+						console.log('chapter-4');
+						flags.fourth = true;
 				}
 
 					break;
-				case Math.floor(duration*.75):
-				if (flags.third === false){
-					console.log('third');
-					flags.third = true;
+				case duration*.80:
+					if (flags.fifth === false){
+						console.log('chapter-5');
+						flags.fifth = true;
 				}
 
 					break;
 				case duration*1:
-				if (flags.finished === false){
-					console.log('finished');
-					flags.finished = true;
+					if (flags.finished === false){
+						console.log('video complete');
+						flags.finished = true;
 				}
 
 					break;
 			}
 
-				if (chapters[progress]){
-					$('.'+ chapters[progress]).addClass('current');
-					$('.'+ chapters[progress]).siblings('.current').removeClass('current').addClass('visited');
+			if (chapters[progress]){
+				$('.'+ chapters[progress]).addClass('current');
+				$('.'+ chapters[progress]).siblings('.current').removeClass('current').addClass('visited');
+				currentChapter = chapters[progress];
+
+			}
+
+			$('.next').on('click', function(){
+				if (currentChapter !== 'chapter-5') {
+
+					$('.current').next('li').trigger('click');
 
 				}
+				else if (currentChapter === 'chapter-5'){
+						myVideo.currentTime = 0;
+						myVideo.play();
+				}
+			});
+
+			$('.prev').on('click', function(){
+				if (currentChapter !== 'chapter-1') {
+
+					$('.current').prev('li').trigger('click');
+
+				}
+				else if (currentChapter === 'chapter-1'){
+						myVideo.currentTime = 20;
+						myVideo.play();
+				}
+			});
+
 	}
 });
